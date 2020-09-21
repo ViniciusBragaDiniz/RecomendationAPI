4927c1929368452842a152ecc154d0aca8b665ed
ViniciusBragaDiniz <38962535+ViniciusBragaDiniz@users.noreply.github.com>
2020-09-21 14:00:36 -0300

    Fixed start

--- app.py
+++ app.py
@@ -186,3 +186,7 @@ def ManageColaborators():
 
 	elif request.method == "GET":
 		return colaborators, 226
+	
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
